87f9f82aa210fb403d0594a7c27fbe9f3e6843c8
Voronovich_dev <alexanderbizv1@gmail.com>
2022-11-14 22:29:06 +0300

    feat: display balance, income and expenses

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Balance = () => {
+  const { incomeTransactions, expenseTransactions } = useSelector(
+    (state) => state.transactionsSlice,
+  );
+
+  const totalIncome = incomeTransactions
+    .reduce((sum, income) => sum + income.incomeAmount, 0)
+    .toFixed(2);
+
+  const totalExpense = expenseTransactions
+    .reduce((sum, expense) => sum + expense.expenseAmount, 0)
+    .toFixed(2);
+
   return (
     <div className="balance">
       <h2>Current Balance</h2>
-      <h3>$10000.00</h3>
+      <h3>${(totalIncome - totalExpense).toFixed(2)}</h3>
       <div className="plus-minus">
         <div className="plus">
           <h3>Income</h3>
-          <p>+$15000.00</p>
+          <p>+${totalIncome}</p>
         </div>
         <div className="minus">
           <h3>Expenses</h3>
-          <p>-$5000.00</p>
+          <p>-${totalExpense}</p>
         </div>
       </div>
     </div>
